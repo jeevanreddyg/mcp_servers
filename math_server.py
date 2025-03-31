eadb64e10b25a6189884e486dbc7d176ec31d8dd
Jeevan <jeevanreddygaddam@gmail.com>
2025-03-31 12:02:16 +0530

    added math server

---
+++ math_server.py
@@ -0,0 +1,20 @@
+from mcp.server.fastmcp import FastMCP
+mcp = FastMCP("math_server", log_level="DEBUG")
+
+@mcp.tool()
+def add_numbers(x: int, y: int) -> int:
+    return x + y
+
+@mcp.tool()
+def substract_numbers(x: int, y: int) -> int:
+    return x - y
+
+@mcp.tool()
+def multiply_numbers(x: int, y: int) -> int:
+    return x * y
+
+if __name__ == "__main__":
+    try:
+        mcp.run(transport="sse")
+    except Exception as e:
+        print(f"Error starting server: {e}")
